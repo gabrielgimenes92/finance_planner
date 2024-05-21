77d3b14ec5c12a829c37a4d8fe876b4890c473f1
Gabriel <gabrielcgimenes@gmail.com>
2024-05-21 16:25:47 -0700

    Adds assetPrefix on next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/finance_planner_frontend",
+    assetPrefix: "/finance_planner_frontend"
 };
 
 export default nextConfig;
